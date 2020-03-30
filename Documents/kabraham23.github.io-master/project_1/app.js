8d8f1794f1853bd5e173def861d9793479ea89ea
Katie <katie@katies-air.lan>
2020-03-30 14:34:19 -0400

    const house fighters added

---
+++ Documents/kabraham23.github.io-master/project_1/app.js
@@ -0,0 +1,74 @@
+console.log('lets duel');
+
+////////////////////
+// psudo-code
+////////////////////
+
+/*--------- HOGWARTS DUELING CLUB ---------*/
+
+// First Screen - 
+// Pick your house
+    // CSS will adapt to house colors
+    // This will also determine which character you play as. Each house/character will have skill levels and atributes.
+    // Gryffindor - Harry Potter
+        // High damage value, higher HP, mid accuracy
+    // Slytherin - Draco Malfoy
+        // High damage value, mid HP, high accuracy 
+            // cruel spells and hexes unlock as you move forward
+    // Ravenclaw - Cho Chang
+        // mid damage value, mid HP, high accuracy *able to heal
+            // healing ability will be timed
+    // Hufflepuff - Cedric Diggory
+        // mid damage value, mid HP, high accuracy, *able to block
+            // blocking ability will be timed - .5 sec to hit block button
+
+
+// Introduction to the game.
+    // Snape is the dueling instructor
+    // Spells used:
+        // Expelliarmus
+        // Bombarda
+        // Stupefy
+        // Petrificus Totalus
+    // key strokes to determine spell used
+
+// First Duel - Lavender Brown
+
+// Second Duel - Luna Lovegood
+
+// Third Duel - Pansy Parkinson
+
+// Fourth Duel - Neville Longbottom
+
+// BOSS BATTLE
+    // Fight Snape. 
+    // Defeat Snape - WIN GAME
+    
+
+// Remus Lupin comes in between duels and offers you chocolate
+    // This resets your HP
+    // Starts next level
+
+const Harry = {
+    attack: 9,
+    defense: 8,
+    accuracy: .6
+};
+
+const Draco = {
+    attack: 6,
+    defense: 9,
+    accuracy: .9
+};
+
+const Cho = {
+    attack: 6,
+    defense: 6,
+    accuracy: .9
+};
+
+const Cedric = {
+    attack: 7,
+    defense: 6,
+    accuracy: .9
+};
